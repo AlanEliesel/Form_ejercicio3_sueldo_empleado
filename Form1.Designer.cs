94332e8dcba1cb87d93702e69fa6d1771337bf18
Alan <Alan@DESKTOP-TG930HR>
2024-02-14 13:29:49 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Form_ejercicio3_sueldo_empleado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_AFP = new System.Windows.Forms.TextBox();
+            this.lbl_afp = new System.Windows.Forms.Label();
+            this.txt_SFS = new System.Windows.Forms.TextBox();
+            this.lbl_sfs = new System.Windows.Forms.Label();
+            this.btn_Procesar = new System.Windows.Forms.Button();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            this.btn_Cerrar = new System.Windows.Forms.Button();
+            this.txt_sueldo_neto = new System.Windows.Forms.TextBox();
+            this.lbl_sueldo_neto = new System.Windows.Forms.Label();
+            this.txt_pago_hora = new System.Windows.Forms.TextBox();
+            this.lbl_pago_hora = new System.Windows.Forms.Label();
+            this.txt_Horas_Trabajadas = new System.Windows.Forms.TextBox();
+            this.lbl_Horas_Trabajadas = new System.Windows.Forms.Label();
+            this.txt_Total_descuento = new System.Windows.Forms.TextBox();
+            this.lbl_total_descuento = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_AFP
+            // 
+            this.txt_AFP.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_AFP.Location = new System.Drawing.Point(265, 190);
+            this.txt_AFP.Name = "txt_AFP";
+            this.txt_AFP.Size = new System.Drawing.Size(213, 30);
+            this.txt_AFP.TabIndex = 3;
+            this.txt_AFP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_afp
+            // 
+            this.lbl_afp.AutoSize = true;
+            this.lbl_afp.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_afp.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_afp.Location = new System.Drawing.Point(136, 194);
+            this.lbl_afp.Name = "lbl_afp";
+            this.lbl_afp.Size = new System.Drawing.Size(45, 22);
+            this.lbl_afp.TabIndex = 2;
+            this.lbl_afp.Text = "AFP";
+            // 
+            // txt_SFS
+            // 
+            this.txt_SFS.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_SFS.Location = new System.Drawing.Point(265, 232);
+            this.txt_SFS.Name = "txt_SFS";
+            this.txt_SFS.Size = new System.Drawing.Size(213, 30);
+            this.txt_SFS.TabIndex = 5;
+            this.txt_SFS.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_sfs
+            // 
+            this.lbl_sfs.AutoSize = true;
+            this.lbl_sfs.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_sfs.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_sfs.Location = new System.Drawing.Point(136, 240);
+            this.lbl_sfs.Name = "lbl_sfs";
+            this.lbl_sfs.Size = new System.Drawing.Size(42, 22);
+            this.lbl_sfs.TabIndex = 4;
+            this.lbl_sfs.Text = "SFS";
+            // 
+            // btn_Procesar
+            // 
+            this.btn_Procesar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_Procesar.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Procesar.Location = new System.Drawing.Point(726, 72);
+            this.btn_Procesar.Name = "btn_Procesar";
+            this.btn_Procesar.Size = new System.Drawing.Size(169, 50);
+            this.btn_Procesar.TabIndex = 8;
+            this.btn_Procesar.Text = "Procesar";
+            this.btn_Procesar.UseVisualStyleBackColor = false;
+            this.btn_Procesar.Click += new System.EventHandler(this.btn_Procesar_Click);
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_Limpiar.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Limpiar.Location = new System.Drawing.Point(726, 151);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(169, 50);
+            this.btn_Limpiar.TabIndex = 9;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = false;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // btn_Cerrar
+            // 
+            this.btn_Cerrar.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btn_Cerrar.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Cerrar.Location = new System.Drawing.Point(726, 236);
+            this.btn_Cerrar.Name = "btn_Cerrar";
+            this.btn_Cerrar.Size = new System.Drawing.Size(169, 50);
+            this.btn_Cerrar.TabIndex = 10;
+            this.btn_Cerrar.Text = "Cerrar";
+            this.btn_Cerrar.UseVisualStyleBackColor = false;
+            this.btn_Cerrar.Click += new System.EventHandler(this.btn_Cerrar_Click);
+            // 
+            // txt_sueldo_neto
+            // 
+            this.txt_sueldo_neto.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_sueldo_neto.Location = new System.Drawing.Point(265, 317);
+            this.txt_sueldo_neto.Name = "txt_sueldo_neto";
+            this.txt_sueldo_neto.Size = new System.Drawing.Size(213, 30);
+            this.txt_sueldo_neto.TabIndex = 14;
+            this.txt_sueldo_neto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_sueldo_neto
+            // 
+            this.lbl_sueldo_neto.AutoSize = true;
+            this.lbl_sueldo_neto.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_sueldo_neto.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_sueldo_neto.Location = new System.Drawing.Point(136, 325);
+            this.lbl_sueldo_neto.Name = "lbl_sueldo_neto";
+            this.lbl_sueldo_neto.Size = new System.Drawing.Size(108, 22);
+            this.lbl_sueldo_neto.TabIndex = 13;
+            this.lbl_sueldo_neto.Text = "Sueldo Neto";
+            // 
+            // txt_pago_hora
+            // 
+            this.txt_pago_hora.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_pago_hora.Location = new System.Drawing.Point(130, 92);
+            this.txt_pago_hora.Name = "txt_pago_hora";
+            this.txt_pago_hora.Size = new System.Drawing.Size(197, 30);
+            this.txt_pago_hora.TabIndex = 16;
+            this.txt_pago_hora.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_pago_hora
+            // 
+            this.lbl_pago_hora.AutoSize = true;
+            this.lbl_pago_hora.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_pago_hora.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_pago_hora.Location = new System.Drawing.Point(163, 44);
+            this.lbl_pago_hora.Name = "lbl_pago_hora";
+            this.lbl_pago_hora.Size = new System.Drawing.Size(128, 22);
+            this.lbl_pago_hora.TabIndex = 15;
+            this.lbl_pago_hora.Text = "Pago por hora";
+            // 
+            // txt_Horas_Trabajadas
+            // 
+            this.txt_Horas_Trabajadas.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Horas_Trabajadas.Location = new System.Drawing.Point(397, 92);
+            this.txt_Horas_Trabajadas.Name = "txt_Horas_Trabajadas";
+            this.txt_Horas_Trabajadas.Size = new System.Drawing.Size(197, 30);
+            this.txt_Horas_Trabajadas.TabIndex = 18;
+            this.txt_Horas_Trabajadas.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_Horas_Trabajadas
+            // 
+            this.lbl_Horas_Trabajadas.AutoSize = true;
+            this.lbl_Horas_Trabajadas.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Horas_Trabajadas.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_Horas_Trabajadas.Location = new System.Drawing.Point(413, 44);
+            this.lbl_Horas_Trabajadas.Name = "lbl_Horas_Trabajadas";
+            this.lbl_Horas_Trabajadas.Size = new System.Drawing.Size(157, 22);
+            this.lbl_Horas_Trabajadas.TabIndex = 17;
+            this.lbl_Horas_Trabajadas.Text = "Horas Trabajadas";
+            // 
+            // txt_Total_descuento
+            // 
+            this.txt_Total_descuento.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Total_descuento.Location = new System.Drawing.Point(279, 268);
+            this.txt_Total_descuento.Name = "txt_Total_descuento";
+            this.txt_Total_descuento.Size = new System.Drawing.Size(199, 30);
+            this.txt_Total_descuento.TabIndex = 20;
+            this.txt_Total_descuento.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_total_descuento
+            // 
+            this.lbl_total_descuento.AutoSize = true;
+            this.lbl_total_descuento.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_total_descuento.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_total_descuento.Location = new System.Drawing.Point(136, 276);
+            this.lbl_total_descuento.Name = "lbl_total_descuento";
+            this.lbl_total_descuento.Size = new System.Drawing.Size(137, 22);
+            this.lbl_total_descuento.TabIndex = 19;
+            this.lbl_total_descuento.Text = "Total descuento";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(961, 413);
+            this.Controls.Add(this.txt_Total_descuento);
+            this.Controls.Add(this.lbl_total_descuento);
+            this.Controls.Add(this.txt_Horas_Trabajadas);
+            this.Controls.Add(this.lbl_Horas_Trabajadas);
+            this.Controls.Add(this.txt_pago_hora);
+            this.Controls.Add(this.lbl_pago_hora);
+            this.Controls.Add(this.txt_sueldo_neto);
+            this.Controls.Add(this.lbl_sueldo_neto);
+            this.Controls.Add(this.btn_Cerrar);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.btn_Procesar);
+            this.Controls.Add(this.txt_SFS);
+            this.Controls.Add(this.lbl_sfs);
+            this.Controls.Add(this.txt_AFP);
+            this.Controls.Add(this.lbl_afp);
+            this.Name = "Form1";
+            this.Text = "Calculaar el sueldo neto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txt_AFP;
+        private System.Windows.Forms.Label lbl_afp;
+        private System.Windows.Forms.TextBox txt_SFS;
+        private System.Windows.Forms.Label lbl_sfs;
+        private System.Windows.Forms.Button btn_Procesar;
+        private System.Windows.Forms.Button btn_Limpiar;
+        private System.Windows.Forms.Button btn_Cerrar;
+        private System.Windows.Forms.TextBox txt_sueldo_neto;
+        private System.Windows.Forms.Label lbl_sueldo_neto;
+        private System.Windows.Forms.TextBox txt_pago_hora;
+        private System.Windows.Forms.Label lbl_pago_hora;
+        private System.Windows.Forms.TextBox txt_Horas_Trabajadas;
+        private System.Windows.Forms.Label lbl_Horas_Trabajadas;
+        private System.Windows.Forms.TextBox txt_Total_descuento;
+        private System.Windows.Forms.Label lbl_total_descuento;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form_ejercicio3_sueldo_empleado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Cerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_Procesar_Click(object sender, EventArgs e)
+        {
+
+            int Pago_Hora = Convert.ToInt32(txt_pago_hora.Text);
+
+            int Horas_Trabajadas = Convert.ToInt32(txt_Horas_Trabajadas.Text);   
+
+            double Sueldo_Bruto = Horas_Trabajadas * Pago_Hora;
+
+            double SFS = Sueldo_Bruto * 3.04 /100;
+            double AFP = Sueldo_Bruto * 2.87 / 100;
+
+            double descuento = SFS + AFP;
+            txt_Total_descuento.Text = descuento.ToString();
+
+            double Sueldo_Neto = Sueldo_Bruto - descuento;
+
+            txt_AFP.Text = Convert.ToString(AFP);
+            txt_SFS.Text = Convert.ToString(SFS);
+
+            txt_sueldo_neto.Text = Convert.ToString(Sueldo_Neto);
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+            txt_AFP.Enabled = false;
+            txt_sueldo_neto.Enabled = false;
+            txt_SFS.Enabled =false;
+
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            txt_sueldo_neto.Clear();
+            txt_AFP.Clear();
+            txt_SFS.Clear();
+            txt_pago_hora.Clear();
+            txt_Horas_Trabajadas.Clear();
+        }
+    }
+}
